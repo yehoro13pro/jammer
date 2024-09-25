97bc079f037c8fc492881e288a40e606ccfbe5b6
yehoro13pro <yeholi@randabergskolen.no>
2024-09-25 09:05:03 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
-input.onButtonPressed(Button.A, function () {
-    sender_id += -1
-    if (sender_id < 0) {
-        sender_id = 5
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    sender_id += 1
-    if (sender_id > 5) {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber < 6) {
         sender_id = 0
+    }
+    if (receivedNumber < 7) {
+        sender_id = 1
+    }
+    if (receivedNumber < 8) {
+        sender_id = 2
+    }
+    if (receivedNumber < 9) {
+        sender_id = 3
+    }
+    if (receivedNumber < 10) {
+        sender_id = 4
+    }
+    if (receivedNumber < 11) {
+        sender_id = 5
     }
 })
 let sender_id = 0
